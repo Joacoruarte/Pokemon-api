f2ade551642d38f420fc9743af7d14d6e3541e5c
Joacoruarte <ruartejoaquin@gmail.com>
2022-03-24 17:50:36 -0300

    heroku

--- PI-Pokemon-main/client/src/components/cards/Card.jsx
+++ PI-Pokemon-main/client/src/components/cards/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Card(props) {
-    const { img , name  , type , id} = props
+    const { img , name  , type , id} = props  
   return ( 
     <Link className='linkCard' key={id} to={`/detail/${id}`}> 
       <div className='card'> 
